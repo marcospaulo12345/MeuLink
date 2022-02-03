15b198cfd5bedec4993d2533fc9059e4f4e5ffca
Marcos <marcospaulo.mpf34@gmail.com>
2022-02-03 14:20:59 -0300

    criado a pagina home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,24 @@
+import {FiLink} from 'react-icons/fi';
+
+import './home.css'
 
 export default function Home() {
     return (
-      <div>
-        <h1>Pagina Home</h1>
+      <div className="container-home">
+        <div className="logo">
+          <img src='/Logo.png' alt='logo' />
+          <h1>My Link</h1>
+          <span>Cole seu link para encurtar</span>
+        </div>
+
+        <div className="area-input">
+          <div>
+            <FiLink size={24} color="#fff" />
+            <input placeholder="Cole seu link aqui..." />
+          </div>
+
+          <button type="button">Encurtar Link</button>
+        </div>
       </div>
     )
   }
